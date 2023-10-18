4c79888691a8016a4f391ace9f8a7940e64b1ead
Janne Rintanen <janne.rintanen@solita.fi>
2023-10-18 08:46:28 +0300

    Add some utils.

---
+++ src/AdventOfCodeTemplateUtil.kt
@@ -0,0 +1,49 @@
+import java.nio.file.Files
+import java.nio.file.Path
+
+object AdventOfCodeTemplateUtil {
+    private fun createDay(dayNum: Int) {
+        val dir = Path.of("src")
+        val dayPrefix = String.format("Day%02d", dayNum)
+        dir.resolve("$dayPrefix.txt").toFile().createNewFile()
+        dir.resolve("${dayPrefix}_test.txt").toFile().createNewFile()
+        val mainFile = dir.resolve("${dayPrefix}.kt").toFile()
+        Files.writeString(mainFile.toPath(), """
+        // See puzzle in https://adventofcode.com/2023/day/$dayNum
+        
+        fun part1(inputLines: List<String>): Int {
+            return 0
+        }
+        
+        fun part2(inputLines: List<String>): Int {
+            return 0
+        }
+        
+        fun main() {
+            val testInput = readInput("${dayPrefix}_test")
+            check(part1(testInput) == 1)
+            
+            val input = readInput("${dayPrefix}")
+            part1(input).println()
+            part2(input).println()
+        }
+        """.trimIndent())
+    }
+
+    fun equals(expected: Number, actual: Number) {
+        check (expected == actual) { "Expected $expected, but was $actual" }
+    }
+
+    fun equals(expected: String, actual: String) {
+        check (expected == actual) { "Expected $expected, but was $actual" }
+    }
+
+    fun debug(str: String, level: Int) {
+        println("${"  ".repeat(level)} $str")
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        createDay(2)
+    }
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,76 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+enum class Direction(val dx: Int, val dy: Int, val symbol: Char) {
+    UP( 0, -1, '^') {
+        override fun turnLeft() = LEFT
+        override fun turnRight() = RIGHT
+    },
+    RIGHT( 1, 0, '>') {
+        override fun turnLeft() = UP
+        override fun turnRight() = DOWN
+    },
+    DOWN( 0, 1, 'v') {
+        override fun turnLeft() = RIGHT
+        override fun turnRight() = LEFT
+    },
+    LEFT( -1, 0, '<') {
+        override fun turnLeft() = DOWN
+        override fun turnRight() = UP
+    };
+
+    abstract fun turnLeft(): Direction
+    abstract fun turnRight(): Direction
+    fun turnOpposite() = turnLeft().turnLeft()
+}
+
+enum class IDirection(val dx: Int, val dy: Int) {
+    NORTH( 0, -1),
+    NORTHEAST( 1, -1),
+    EAST( 1, 0),
+    SOUTHEAST( 1, 1),
+    SOUTH( 0, 1),
+    SOUTHWEST( -1, 1),
+    WEST( -1, 0),
+    NORTHWEST( -1, -1)
+}
+
+data class Coord(val x: Int, val y: Int) {
+    fun neighbors(): List<Coord> = Direction.values().map { this.move(it) }
+    fun diagonalNeighbors(): List<Coord> = IDirection.values().map { this.move(it) }
+    fun move(direction: IDirection, amount: Int = 1) =
+        Coord(x + amount * direction.dx, y + amount * direction.dy)
+    fun move(direction: Direction, amount: Int = 1) =
+        Coord(x + amount * direction.dx, y + amount * direction.dy)
+
+    fun moveUntil(direction: Direction, predicate: (Coord) -> Boolean): Coord {
+        val maxMoves = 10000
+        var newCoord = this
+
+        for (n in 0 until maxMoves) {
+            val potentialNewCoord = newCoord.move(direction)
+            if (predicate(potentialNewCoord)) {
+                return newCoord
+            }
+            newCoord = potentialNewCoord
+        }
+
+        throw IllegalArgumentException("Too many (> $maxMoves) moves!")
+    }
+
+    override fun toString() = "($x, $y)"
+}
+
+fun drawGrid(coords: Set<Coord>, tileSymbolAt: (Coord) -> Char) {
+    val yRange = coords.minBy { it.y }.y .. coords.maxBy { it.y }.y
+    val xRange = coords.minBy { it.x }.x .. coords.maxBy { it.x }.x
+
+    for (y in yRange) {
+        for (x in xRange) {
+            val coord = Coord(x, y)
+            print(tileSymbolAt(coord))
+        }
+        println()
+    }
+}
