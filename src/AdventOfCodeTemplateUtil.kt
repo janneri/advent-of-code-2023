f70555bd92993f90197ebed49b9b91e4030c5ec6
Janne Rintanen <janne.rintanen@solita.fi>
2023-10-22 15:48:16 +0300

    Add input download to util

--- src/AdventOfCodeTemplateUtil.kt
+++ src/AdventOfCodeTemplateUtil.kt
@@ -1,13 +1,24 @@
+import java.io.File
+import java.net.CookieHandler
+import java.net.CookieManager
+import java.net.HttpCookie
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.nio.file.Files
 import java.nio.file.Path
+import java.time.Duration
+
 
 object AdventOfCodeTemplateUtil {
     private fun createDay(dayNum: Int) {
         val dir = Path.of("src")
         val dayPrefix = String.format("Day%02d", dayNum)
-        dir.resolve("$dayPrefix.txt").toFile().createNewFile()
+        val inputTxtFile = dir.resolve("$dayPrefix.txt").toFile()
         dir.resolve("${dayPrefix}_test.txt").toFile().createNewFile()
         val mainFile = dir.resolve("${dayPrefix}.kt").toFile()
+
         Files.writeString(mainFile.toPath(), """
         // See puzzle in https://adventofcode.com/2023/day/$dayNum
         
@@ -28,6 +39,8 @@ object AdventOfCodeTemplateUtil {
             part2(input).println()
         }
         """.trimIndent())
+
+        Files.writeString(inputTxtFile.toPath(), downloadInput(dayNum))
     }
 
     fun equals(expected: Number, actual: Number) {
@@ -40,6 +53,30 @@ object AdventOfCodeTemplateUtil {
 
     fun debug(str: String, level: Int) {
         println("${"  ".repeat(level)} $str")
+    }
+
+    private fun getHttpClient(): HttpClient {
+        CookieHandler.setDefault(CookieManager())
+        val cookieManager = CookieHandler.getDefault() as CookieManager
+        val sessionCookieValue = File(System.getProperty("user.home"),".adventofcode.session").readText().trim()
+        val aocSessionCookie = HttpCookie("session", sessionCookieValue).apply {
+            path = "/"
+            version = 0
+        }
+        cookieManager.cookieStore.add(URI("https://adventofcode.com"), aocSessionCookie)
+        return HttpClient.newBuilder()
+            .cookieHandler(cookieManager)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build()
+    }
+
+    private fun downloadInput(dayNum: Int): String? {
+        val client = getHttpClient()
+        val req = HttpRequest.newBuilder()
+            .uri(URI.create("https://adventofcode.com/2022/day/$dayNum/input"))
+            .GET().build()
+
+        return client.send(req, HttpResponse.BodyHandlers.ofString()).body().trim()
     }
 
     @JvmStatic
